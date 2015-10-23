4c8a933d13dc7d166ecf083503aa64d03795047a
Ismail Syed <isyed867@gmail.com>
2015-10-22 21:18:19 -0400

    updated meteor and added android support :)

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,25 +1,9 @@
-// Template.home.onRendered(function () {
-// 	debugger; 
-//  	var navbarAccountsBtn = $('#navbar-btn-accounts');
-// 	if( !navbarAccountsBtn.parent().hasClass('open') ){
-// 		var loginSignInLink = $('#login-sign-in-link');
-// 		loginSignInLink.click();
-// 		// loginSignInLink.css('display', 'none')
-// 	}
-// 	else{
-// 		return;
-// 	}
-// });
+Template.home.rendered = function() {
+    if(!this._rendered) {
+      this._rendered = true;
+      	$('#login-sign-in-link').click();
+	   $('#login-dropdown-list').addClass('open');
+	   // alert('rendered');
 
-// Template.home.onRendered(function () {
-//   // Template.loginButtons.onRendered(function () {
-
-//     $('#login-sign-in-link').click();
-//   // });
-// });
-
-Template.home.rendered = function () {
-  this.autorun(function () {
-    $('#login-sign-in-link').click();
-  });
-};
+    }
+}
